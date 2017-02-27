927612dac2078e49a7a603bf024ba254349004f5
Elliot <elliot.ayrey@gmail.com>
2017-02-27 21:50:56 +1300

    Second commit - expanding the script.

---
+++ CheckMark.py
@@ -0,0 +1,5 @@
+def checkMark(num):
+    result = "FAIL"
+    if num >= 50:
+        result = "PASS"
+    return result
--- Main.py
+++ Main.py
@@ -1,14 +1,18 @@
 import View
+import CheckMark
 
 class Main(object):
-    def __init__(self, name):
+    def __init__(self, name, mark):
         self.name = name
+        self.mark = mark
 
     def go(self):
         View.start()
         View.say("Hello, " + self.name)
+        result = CheckMark.checkMark(self.mark)
+        View.say("Your result is " + result)
         View.stop()
 
-stuff = Main("Elliot")
+stuff = Main("Elliot", 51)
 
 stuff.go()
